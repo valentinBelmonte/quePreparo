7fcdba469aa1c9528824313c53bb11c77f31f14e
valentinBelmonte <valen.belmonte@gmail.com>
2017-03-31 00:25:07 -0300

    se terminan los if, se arma el form, se pones el boton del form, y se corrigen los values del form a lo que esta en el ID de ingredientas

---
+++ quePreparoConCSharp/quePreparoConCSharp/Controllers/RecetaController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quePreparoConCSharp.Controllers
+{
+    public class RecetaController : Controller
+    {
+        [HttpPost]
+        public ActionResult BuscarReceta(/*ACA VA UN INT POR CADA VALOR DE LOS VALUES DEL FORM*/)
+        {
+            List<int> listaIng = new List<int>();
+
+            /*HAY QUE HACER UN IF POR CADA VALOR DE INT QUE HAY EN LA LISTA, ERGO POR CADA VALUE DEL FORM*/
+
+
+
+            if (1 != 0)
+            {
+                listaIng.Add(int 1) = 1;
+            }
+            if (2 != 0)
+            {
+                listaIng.Add(int 2) = 2;
+            }
+            if (3 != 0)
+            {
+                listaIng.Add(int 3) = 3;
+            }
+            if (4 != 0)
+            {
+                listaIng.Add(int 4) = 4;
+            }
+            if (5 != 0)
+            {
+                listaIng.Add(int 5) = 5;
+            }
+            if (6 != 0)
+            {
+                listaIng.Add(int 6) = 6;
+            }
+            if (7 != 0)
+            {
+                listaIng.Add(int 7) = 7;
+            }
+            if (8 != 0)
+            {
+                listaIng.Add(int 8) = 8;
+            }
+            if (9 != 0)
+            {
+                listaIng.Add(int 9) = 9;
+            }
+            if (10 != 0)
+            {
+                listaIng.Add(int 10) = 10;
+            }
+            if (11 != 0)
+            {
+                listaIng.Add(int 11) = 11;
+            }
+            if (12 != 0)
+            {
+                listaIng.Add(int 12) = 12;
+            }
+            if (13 != 0)
+            {
+                listaIng.Add(int 13) = 13;
+            }
+            if (14 != 0)
+            {
+                listaIng.Add(int 14) = 14;
+            }
+            if (15 != 0)
+            {
+                listaIng.Add(int 15) = 15;
+            }
+            if (16 != 0)
+            {
+                listaIng.Add(int 16) = 16;
+            }
+            if (17 != 0)
+            {
+                listaIng.Add(int 17) = 17;
+            }
+            if (18 != 0)
+            {
+                listaIng.Add(int 18) = 18;
+            }
+            if (19 != 0)
+            {
+                listaIng.Add(int 19) = 19;
+            }
+            if (20 != 0)
+            {
+                listaIng.Add(int 20) = 20;
+            }
+            if (21 != 0)
+            {
+                listaIng.Add(int 21) = 21;
+            }
+            if (22 != 0)
+            {
+                listaIng.Add(int 22) = 22;
+            }
+            if (23 != 0)
+            {
+                listaIng.Add(int 23) = 23;
+            }
+            if (24 != 0)
+            {
+                listaIng.Add(int 24) = 24;
+            }
+            if (25 != 0)
+            {
+                listaIng.Add(int 25) = 25;
+            }
+            if (26 != 0)
+            {
+                listaIng.Add(int 26) = 26;
+            }
+            if (27 != 0)
+            {
+                listaIng.Add(int 27) = 27;
+            }
+            if (28 != 0)
+            {
+                listaIng.Add(int 28) = 28;
+            }
+            if (29 != 0)
+            {
+                listaIng.Add(int 29) = 29;
+            }
+            if (30 != 0)
+            {
+                listaIng.Add(int 30) = 30;
+            }
+            if (31 != 0)
+            {
+                listaIng.Add(int 31) = 31;
+            }
+            if (32 != 0)
+            {
+                listaIng.Add(int 32) = 32;
+            }
+            if (33 != 0)
+            {
+                listaIng.Add(int 33) = 33;
+            }
+            if (34 != 0)
+            {
+                listaIng.Add(int 34) = 34;
+            }
+            if (35 != 0)
+            {
+                listaIng.Add(int 35) = 35;
+            }
+            if (36 != 0)
+            {
+                listaIng.Add(int 36) = 36;
+            }
+            if (37 != 0)
+            {
+                listaIng.Add(int 37) = 37;
+            }
+            if (38 != 0)
+            {
+                listaIng.Add(int 38) = 38;
+            }
+            if (39 != 0)
+            {
+                listaIng.Add(int 39) = 39;
+            }
+            if (40 != 0)
+            {
+                listaIng.Add(int 40) = 40;
+            }
+            if (41 != 0)
+            {
+                listaIng.Add(int 41) = 41;
+            }
+            if (42 != 0)
+            {
+                listaIng.Add(int 42) = 42;
+            }
+            if (43 != 0)
+            {
+                listaIng.Add(int 43) = 43;
+            }
+            if (44 != 0)
+            {
+                listaIng.Add(int 44) = 44;
+            }
+            if (45 != 0)
+            {
+                listaIng.Add(int 45) = 45;
+            }
+            if (46 != 0)
+            {
+                listaIng.Add(int 46) = 46;
+            }
+            if (47 != 0)
+            {
+                listaIng.Add(int 47) = 47;
+            }
+            if (48 != 0)
+            {
+                listaIng.Add(int 48) = 48;
+            }
+
+
+
+            /*SE CREA UN MANAGER PARA MANEJAR LA CONSULTA*/
+
+            Manger Mimanager = new Manager
+
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
